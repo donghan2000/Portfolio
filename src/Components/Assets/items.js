de5f852e206284fa1d742028d678e9ac2f50edce
Donghan <32202779+donghan2000@users.noreply.github.com>
2025-03-08 22:31:59 +0800

    Update items.js

--- src/Components/Assets/items.js
+++ src/Components/Assets/items.js
@@ -9,10 +9,103 @@ export const items = [
     firstParagraph: "I take immense pride in the website I had created for my previous portfolio. It stands as a remarkable reflection of my journey and growth throughout the years. Its inclusion in this gallery of work is a testament to the progress I have made in my journey.  ",
     secondParagraph: "The website was meticulously designed, featuring captivating backgrounds aimed at catching the attention of potential employers. It was a deliberate effort to showcase my skills and capabilities in a visually stunning and memorable way",
     thirdParagraph: "Check out the fully coded responsive site  in the link below",
-    linkText: "WEBSITE LINK",
-    linkGoto: "http://portfolio.donghan.co"
+    // linkText: "WEBSITE LINK",
+    // linkGoto: "http://portfolio.donghan.co"
 
 
+
+  }, {
+    imageUrl: "url(./img/thumbnails/ntucfirstcampus-thumb.png)",
+    height: 300,
+    Title: "NTUC FirstCampus Preschool Listing",
+    workContentImage: "./img/thumbnails/ntucfirstcampus-content.png",
+    firstParagraph: "To showcase all the preschool centres under NTUC FirstCampus",
+    secondParagraph: "I was responsible for integration of OneMap and Toolset , the functions as well as the layout of the site",
+    thirdParagraph: "Check out the project in the link below",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://www.ntucfirstcampus.com/preschool/preschool-centres/",
+
+  },
+  {
+    imageUrl: "url(./img/thumbnails/keppel-annual-report-2023-thumb.png)",
+    height: 300,
+    Title: "Keppel Annual Report 2023",
+    workContentImage: "./img/thumbnails/keppel-annual-report-2023-content.png",
+    firstParagraph: "Website for Keppel Annual Report 2023",
+    secondParagraph: "I was responsible for the layout and content of the website",
+    thirdParagraph: "Check out the project in the link below",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://www.keppel.com/annualreport2023/",
+
+  },
+  {
+    imageUrl: "url(./img/thumbnails/idea-festival-2024-thumb.png)",
+    height: 300,
+    Title: "Ideas Festival Singapore 2024",
+    workContentImage: "./img/thumbnails/idea-festival-2024-content.png",
+    firstParagraph: "Website for Ideas Festrival Singapore 2024 using the Isomer CMS.",
+    secondParagraph: "I was responsible for the layout and content of the website.",
+    thirdParagraph: "Check out the project in the link below",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://www.ideasfestival.edu.sg/",
+
+  },
+  {
+    imageUrl: "url(./img/thumbnails/ntu-thumb.png)",
+    height: 300,
+    Title: "NTU Career Tracks 2024",
+    workContentImage: "./img/thumbnails/ntu-content.png",
+    firstParagraph: "NTU website that is an exclusive online career guide for all NTU students to prepare you to thrive and transform in the world of work.",
+    secondParagraph: "I was responsible for the layout and content of the website.",
+    thirdParagraph: "Check out the project in the link below",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://www.careertracks.edu.sg/",
+
+  },
+  {
+    imageUrl: "url(./img/thumbnails/nparks-annual-report-2022/23-thumb.png)",
+    height: 300,
+    Title: "National Parks AR 2022/23",
+    workContentImage: "./img/thumbnails/nparks-annual-report-2022/23-content.png",
+    firstParagraph: "Website for Nparks Annual Report 2022/23",
+    secondParagraph: "I was responsible for the layout and content of the website",
+    thirdParagraph: "Check out the project in the link below",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://www.whooshpro.com/works/national-parks-ar-2022-23/",
+
+  },
+  {
+    imageUrl: "url(./img/thumbnails/scar-annual-report-2022-thumb.png)",
+    height: 300,
+    Title: "SG Courts AR 2022",
+    workContentImage: "./img/thumbnails/scar-annual-report-2022-content.png",
+    firstParagraph: "Website for SG Courts Annual Report 2022",
+    secondParagraph: "I was responsible for the layout and content of the website",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://www.judiciary.gov.sg/AnnualReport2022/",
+
+  },
+  {
+    imageUrl: "url(./img/thumbnails/shopandshare-thumb.png)",
+    height: 300,
+    Title: "MediaCorp Shop and Share",
+    workContentImage: "./img/thumbnails/shopandshare-content.png",
+    firstParagraph: "Website that is a corporate social responsibility initiative, in collaboration with Wondershop.sg, that allows you to purchase gifts online and donate them to beneficiaries in need, including people with special needs, low-income families, and the physically challenged.",
+    secondParagraph: "I was responsible for the layout and content of the website",
+    thirdParagraph: "Check out the project in the link below",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://www.whooshpro.com/works/mediacorp-shop-and-share/",
+
+  },
+  {
+    imageUrl: "url(./img/thumbnails/nhg-annual-report-thumb.png)",
+    height: 300,
+    Title: "NHG Corporate Yearbook FY2022/23",
+    workContentImage: "./img/thumbnails/nhg-annual-report-content.png",
+    firstParagraph: "Website for NHG Corporate Yearbook FY2022/23",
+    secondParagraph: "I was responsible for the layout and content of the website",
+    linkText: "PROJECT LINK",
+    linkGoto: "https://corp.nhg.com.sg/Documents/FY22%20FY23%20NHG%20Corporate%20Yearbook%20Digital/index.html",
 
   },
   {
@@ -24,8 +117,8 @@ export const items = [
     firstParagraph: "Fusion 2020 was a project that was showcased as part of the Nyp SIDM graduation show in 2020, during the COVID-19 pandemic. The physical exhibition was cancelled, but the project was made accessible via a dedicated website, serving as a hub for companies seeking to recruit prospective students and for graduates seeking potential employers.",
     secondParagraph: "I was responsible for coding the website, which also featured a collection of students' work and project descriptions. Despite last-minute adjustments due to the cancellation, the project was a success.",
     thirdParagraph: "Check out the fully coded responsive site in the link below",
-    linkText: "WEBSITE LINK",
-    linkGoto: "http://fusion.donghan.co"
+    // linkText: "WEBSITE LINK",
+    // linkGoto: "http://fusion.donghan.co"
 
   },
   {
@@ -36,8 +129,8 @@ export const items = [
     firstParagraph: "My group and I developed 'UPNetty', a health tracking device that monitors urine and feces from the comfort of home to ensure our well-being and prevent life-threatening medical conditions.",
     secondParagraph: "I did everything coding related in this project , from the website to the app . The website is fully reI coded the website and app for the 'UPNetty' project, which tracks health through urine and feces. The website is responsive and the contact form is functional. The app displays dynamic information using PHP and MySQL. Check out the fully coded responsive site in the link below",
     thirdParagraph: "Check out the website in the link below",
-    linkText: "WEBSITE LINK",
-    linkGoto: "http://upnetty.donghan.co"
+    // linkText: "WEBSITE LINK",
+    // linkGoto: "http://upnetty.donghan.co"
   },
   {
     imageUrl: "url(./img/thumbnails/Solace-Thumb.png)",
@@ -48,7 +141,7 @@ export const items = [
     secondParagraph: "A protoype of the app was done using adobe xd to sitmulate what is like to have the functioning app",
     thirdParagraph: "Check out the project in the link below",
     linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/solace.pdf",
+    linkGoto: "/solace.pdf",
     linkSecondText: "XD PROTOTYPE APP LINK",
     linkSecondGoto: "https://xd.adobe.com/view/ed617b9d-d25a-4fca-727e-dc14762e96f9-9b50/",
 
@@ -61,8 +154,8 @@ export const items = [
     firstParagraph: "I coded a personal book collection website for a project, fueled by my passion for reading.",
     secondParagraph: "I created a book collection website with a simple design, focusing on essential elements for users to easily find and record their books. I drew inspiration from other book collection websites.",
     thirdParagraph: "Check out the website in the link below",
-    linkText: "WEBSITE LINK",
-    linkGoto: "http://shosetsu.donghan.co"
+    // linkText: "WEBSITE LINK",
+    // linkGoto: "http://shosetsu.donghan.co"
 
   },
   {
@@ -73,8 +166,8 @@ export const items = [
     firstParagraph: "The app's purpose is to provide users a convenient way to find kayaking locations in Singapore.",
     secondParagraph: "The app design reflects the changing weather and time, with the background transitioning from violet to blue, brown, and finally indigo as the day progresses. The mobile app was developed and incorporated real-time weather data using APIs.",
     thirdParagraph: "Check out the website in the link below",
-    linkText: "WEBSITE LINK",
-    linkGoto: "http://wekayak.donghan.co"
+    // linkText: "WEBSITE LINK",
+    // linkGoto: "http://wekayak.donghan.co"
   },
   {
     imageUrl: "url(./img/thumbnails/Worm-Thumb.png)",
@@ -84,8 +177,8 @@ export const items = [
     firstParagraph: "For this project , I was to create an educational game project to provide awareness on somethings positive.",
     secondParagraph: "I wanted have some nostalgia in doing a simple game so I decided to revamp and create a game based around the classic snake game",
     thirdParagraph: "Check out the game in the link below",
-    linkText: "WEBSITE LINK",
-    linkGoto: "http://worm.donghan.co"
+    // linkText: "WEBSITE LINK",
+    // linkGoto: "http://worm.donghan.co"
   },
   {
     imageUrl: "url(./img/thumbnails/Cah-Thumb.png)",
@@ -127,7 +220,7 @@ export const items = [
     secondParagraph: "Our product was designed with the teacher's convenience in mind. To address their frustrations, we gathered their input and developed the idea of using spectacles to streamline the marking process.",
     thirdParagraph: "Check out the project in the link below",
     linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/smartime.pdf",
+    linkGoto: "/smartime.pdf",
 
   },
   {
@@ -139,7 +232,7 @@ export const items = [
     secondParagraph: "I contributed illustrations and designed interactive elements for the storybook. The aim was to create engaging and memorable experiences for users, emphasizing the impact of littering on marine wildlife.",
     thirdParagraph: "Check out the project in the link below",
     linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/storybook.pdf",
+    linkGoto: "/storybook.pdf",
 
   },
   {
@@ -151,67 +244,8 @@ export const items = [
     secondParagraph: "I was responsible for implementing the chatbot in a project aimed at addressing the issue of suicide prevention. Extensive research and user testing was conducted to refine the conversational flow of the chatbot, which was built using Slack integration as a prototype.",
     thirdParagraph: "Check out the project in the link below",
     linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/vapour.pdf",
+    linkGoto: "/vapour.pdf",
 
   },
-    {
-    imageUrl: "url(./img/thumbnails/Vapour-Thumb.png)",
-    height: 300,
-    Title: "Vapour",
-    workContentImage: "./img/thumbnails/Vapour-Content.png",
-    firstParagraph: "Our project aimed to address pressing social issues and we chose to focus on suicide prevention. After conducting research, we believed that implementing an AI-powered chatbot could effectively reduce the number of suicides.",
-    secondParagraph: "I was responsible for implementing the chatbot in a project aimed at addressing the issue of suicide prevention. Extensive research and user testing was conducted to refine the conversational flow of the chatbot, which was built using Slack integration as a prototype.",
-    thirdParagraph: "Check out the project in the link below",
-    linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/vapour.pdf",
 
-  },
-    {
-    imageUrl: "url(./img/thumbnails/Vapour-Thumb.png)",
-    height: 300,
-    Title: "Vapour",
-    workContentImage: "./img/thumbnails/Vapour-Content.png",
-    firstParagraph: "Our project aimed to address pressing social issues and we chose to focus on suicide prevention. After conducting research, we believed that implementing an AI-powered chatbot could effectively reduce the number of suicides.",
-    secondParagraph: "I was responsible for implementing the chatbot in a project aimed at addressing the issue of suicide prevention. Extensive research and user testing was conducted to refine the conversational flow of the chatbot, which was built using Slack integration as a prototype.",
-    thirdParagraph: "Check out the project in the link below",
-    linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/vapour.pdf",
-
-  },
-    {
-    imageUrl: "url(./img/thumbnails/Vapour-Thumb.png)",
-    height: 300,
-    Title: "Vapour",
-    workContentImage: "./img/thumbnails/Vapour-Content.png",
-    firstParagraph: "Our project aimed to address pressing social issues and we chose to focus on suicide prevention. After conducting research, we believed that implementing an AI-powered chatbot could effectively reduce the number of suicides.",
-    secondParagraph: "I was responsible for implementing the chatbot in a project aimed at addressing the issue of suicide prevention. Extensive research and user testing was conducted to refine the conversational flow of the chatbot, which was built using Slack integration as a prototype.",
-    thirdParagraph: "Check out the project in the link below",
-    linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/vapour.pdf",
-
-  },
-    {
-    imageUrl: "url(./img/thumbnails/Vapour-Thumb.png)",
-    height: 300,
-    Title: "Vapour",
-    workContentImage: "./img/thumbnails/Vapour-Content.png",
-    firstParagraph: "Our project aimed to address pressing social issues and we chose to focus on suicide prevention. After conducting research, we believed that implementing an AI-powered chatbot could effectively reduce the number of suicides.",
-    secondParagraph: "I was responsible for implementing the chatbot in a project aimed at addressing the issue of suicide prevention. Extensive research and user testing was conducted to refine the conversational flow of the chatbot, which was built using Slack integration as a prototype.",
-    thirdParagraph: "Check out the project in the link below",
-    linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/vapour.pdf",
-
-  },
-    {
-    imageUrl: "url(./img/thumbnails/Vapour-Thumb.png)",
-    height: 300,
-    Title: "Vapour",
-    workContentImage: "./img/thumbnails/Vapour-Content.png",
-    firstParagraph: "Our project aimed to address pressing social issues and we chose to focus on suicide prevention. After conducting research, we believed that implementing an AI-powered chatbot could effectively reduce the number of suicides.",
-    secondParagraph: "I was responsible for implementing the chatbot in a project aimed at addressing the issue of suicide prevention. Extensive research and user testing was conducted to refine the conversational flow of the chatbot, which was built using Slack integration as a prototype.",
-    thirdParagraph: "Check out the project in the link below",
-    linkText: "PROJECT LINK",
-    linkGoto: "http://donghan.co/vapour.pdf",
-
-  },
 ]
