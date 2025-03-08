d493cd383f7d60d4b95be2b0644fd9226b4de8cf
Donghan <32202779+donghan2000@users.noreply.github.com>
2025-03-08 22:33:04 +0800

    Update Copyrights.jsx

--- src/Components/Copyrights.jsx
+++ src/Components/Copyrights.jsx
@@ -5,7 +5,7 @@ export default function Copyright() {
         <div className='copy-div'>
             <div className='copy-text'>
                 <img className='copy-icon' src='./img/bx-copyright.svg' alt='copySection' />
-                <p>DONG HAN 2023</p>
+                <p>DONG HAN 2025</p>
             </div>
         </div>
     </>
